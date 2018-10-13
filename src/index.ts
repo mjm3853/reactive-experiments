80c7e7f945a4d9fc7c03d753e3246ea14485b985
Matthew McGrath <matthew.mcgrath12@gmail.com>
2018-10-13 17:04:19 -0400

    some functions

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,69 @@
-import { fromEvent } from 'rxjs';
-import { startWith, map } from 'rxjs/operators';
+import { fromEvent, of } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
+import { map, merge, startWith } from 'rxjs/operators';
 
 const refreshButton = document.querySelector('.refresh');
-const users = document.querySelector('.users')
+const users = document.querySelector('.users');
 const refreshClickStream = fromEvent(refreshButton, 'click');
 const requestStream = refreshClickStream.pipe(
     startWith('startup click'),
     map(() => {
-        let randomOffset = Math.floor(Math.random() * 500);
+        const randomOffset = Math.floor(Math.random() * 500);
         return `https://api.github.com/users?since=${randomOffset}`;
-    })
-)
+    }),
+);
+function addParagraph(body: string) {
+    const p = document.createElement('p');
+    p.innerHTML = body;
+    users.appendChild(p);
+}
+
+function handleRefresh(body: any[]) {
+    if (body) {
+        body.forEach((item) => {
+            addParagraph(item.login);
+        });
+    } else {
+        clearUsers();
+    }
+}
+
+function handleError(body: string) {
+    if (body) {
+        addParagraph(body);
+        clearUsers();
+    } else {
+        clearUsers();
+    }
+}
+
+function clearUsers() {
+    while (users.hasChildNodes()) {
+        users.removeChild(users.firstChild);
+    }
+}
 
 requestStream.subscribe(
     (requestUrl) => {
         const responseStream = ajax.getJSON(requestUrl).pipe(
             map((res: any[]) => {
-                return res[0];
-            })
+                return res.slice(0, 3);
+            }),
+            merge(
+                of(null),
+            ),
         );
 
         responseStream.subscribe(
-            (res) => {
-                const p = document.createElement("p");
-                p.innerHTML = res.login;
-                users.appendChild(p);
+            (res: any[]) => {
+                handleRefresh(res);
             },
             (err) => {
-                console.log(err);
-            }
+                handleError(err);
+            },
         );
     },
-    (err) => console.log(err),
+    (err) => {
+        handleError(err);
+    },
 );
