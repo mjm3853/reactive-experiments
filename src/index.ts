44822150209ac7714e8d8c64b29df9793dd099a2
Matthew McGrath <matthew.mcgrath12@gmail.com>
2018-10-13 15:10:23 -0400

    response stream

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,35 @@
-import { fromEvent, of } from 'rxjs';
+import { fromEvent } from 'rxjs';
 import { startWith, map } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 
 const refreshButton = document.querySelector('.refresh');
+const users = document.querySelector('.users')
 const refreshClickStream = fromEvent(refreshButton, 'click');
 const requestStream = refreshClickStream.pipe(
     startWith('startup click'),
-    map(
-        () => {
-            let randomOffset = Math.floor(Math.random() * 500);
-            return `https://api.github.com/users?since=${randomOffset}`;
-        }
-    )
+    map(() => {
+        let randomOffset = Math.floor(Math.random() * 500);
+        return `https://api.github.com/users?since=${randomOffset}`;
+    })
 )
 
 requestStream.subscribe(
     (requestUrl) => {
-        ajax.getJSON(requestUrl).subscribe(
-            (res) => console.log(res),
-            (err) => console.log(err),
+        const responseStream = ajax.getJSON(requestUrl).pipe(
+            map((res: any[]) => {
+                return res[0];
+            })
+        );
+
+        responseStream.subscribe(
+            (res) => {
+                const p = document.createElement("p");
+                p.innerHTML = res.login;
+                users.appendChild(p);
+            },
+            (err) => {
+                console.log(err);
+            }
         );
     },
     (err) => console.log(err),
